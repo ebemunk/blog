ca096da6469e7941ef2fe540dc1ea50ee9587fde
ebemunk <ebemunk@gmail.com>
2017-02-20 01:22:41 -0800

    watson start

---
+++ static/lost-transcript-mining/collect/commands/watson.js
@@ -0,0 +1,13 @@
+/*
+tone analyzer
+	first 1000 calls free
+	up to 128kb text or 1000 sentences
+
+personality insights
+	first 100 calls free
+	20mb
+
+alchemy api
+	free plan, 1000 api plans per day
+
+*/
