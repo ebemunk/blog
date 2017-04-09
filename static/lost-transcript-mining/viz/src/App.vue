096d8db18468073c820b840dc0dde4efead619d4
Bugra Firat <bfirat@xmatters.com>
2017-04-09 01:05:51 -0700

    move computed to store getters + appmetrics

--- static/lost-transcript-mining/viz/src/App.vue
+++ static/lost-transcript-mining/viz/src/App.vue
@@ -9,10 +9,10 @@
 		:nodesProp="charOccNodes"
 		:linksProp="charCooccurrence"
 	/>
-	<!-- <BarChart
+	<BarChart
 		:data="totalLines"
 		:class="$style.totalLines"
-	/> -->
+	/>
 </div>
 </template>
 
@@ -61,6 +61,9 @@ export default {
 		...mapGetters([
 			'episodeSelection',
 			'selectedEpisodes',
+			'totalLines',
+			'charCooccurrence',
+			'charOccNodes',
 		]),
 		selected: function () {
 			const {
@@ -72,77 +75,6 @@ export default {
 				getKey(_.last(selectedEpisodes))
 			]
 		},
-		totalLines: function () {
-			// console.log('computing');
-			const keepKeys = this.selectedEpisodes.map(d => d.key)
-			const plz = _(data.totalLines)
-			.filter(d => {
-				// console.log('wa', d);
-				return keepKeys.includes(`S${pad2(d.season)}-E${pad2(d.episode)}`)
-			})
-			.groupBy('char_name')
-			.map((char, key) => ({
-				char_name: key,
-				lines: char.reduce((total, val) => {
-					return total + val.lines
-				}, 0)
-			}))
-			.orderBy('lines', 'desc')
-			.value()
-			// console.log(plz);
-			return plz
-		},
-		charOccNodes: function () {
-			const { charCooccurrence } = this
-			const nodes = _.uniq(charCooccurrence.map(d => d.source).concat(charCooccurrence.map(d => d.target)))
-			.map(d => {
-				const r = charCooccurrence.filter(item => item.target === d).length
-				return {
-					id: d,
-					r: Math.max(r+2, 3)
-				}
-			})
-
-			return nodes
-		},
-		charCooccurrence: function () {
-			const keepKeys = this.selectedEpisodes.map(d => d.key)
-			const plz = _(data.charCooccurrence)
-			.filter(d => {
-				// console.log('wa', d);
-				return keepKeys.includes(`S${pad2(d.season)}-E${pad2(d.episode)}`)
-			})
-			// .tap(plzl => console.log('ya', plzl))
-			.reduce((map, val) => {
-				const key = _.pick(val, ['from_char', 'to_char'])
-				if( ! map.has(key) ) {
-					map.set(key, val.val)
-				} else {
-					const c = map.get(key)
-					map.set(key, c+val.val)
-				}
-				return map
-			}, new Map())
-
-			const plz2 = Array.from(plz)
-			.map(d => {
-				return {
-					source: d[0].from_char,
-					target: d[0].to_char,
-					value: d[1]
-				}
-			})
-
-			// const plz2 = _.map(plz,(v, key) => {
-			// 	console.log('wa', v, key);
-			// 	return {
-			// 		...key,
-			// 		count: v
-			// 	}
-			// })
-			console.log('val', plz2);
-			return plz2
-		}
 	},
 	mounted() {
 	},
--- static/lost-transcript-mining/viz/src/store/getters.js
+++ static/lost-transcript-mining/viz/src/store/getters.js
@@ -1,3 +1,86 @@
+import _ from 'lodash'
+import Measure from 'appmetrics.js'
+
+import * as data from '../data'
+
+const pad2 = _.partialRight(_.padStart, 2, 0)
+
 export const seasonEpisodes = state => state.seasonEpisodes
+
 export const episodeSelection = state => state.episodeSelection
+
 export const selectedEpisodes = state => state.seasonEpisodes.slice(...state.episodeSelection)
+
+export const totalLines = (state, getters) => {
+  const m = new Measure('totalLines')
+  m.start()
+  const keepKeys = getters.selectedEpisodes.map(d => d.key)
+  const plz = _(data.totalLines)
+  .filter(d => {
+    return keepKeys.includes(`S${pad2(d.season)}-E${pad2(d.episode)}`)
+  })
+  .groupBy('char_name')
+  .map((char, key) => ({
+    char_name: key,
+    lines: char.reduce((total, val) => {
+      return total + val.lines
+    }, 0)
+  }))
+  .orderBy('lines', 'desc')
+  .value()
+  m.end().log()
+  return plz
+}
+
+
+export const charCooccurrence = (state, getters) => {
+  const m = new Measure('charCooccurrence')
+  m.start()
+  const keepKeys = getters.selectedEpisodes.map(d => d.key)
+  const plz = _(data.charCooccurrence)
+  .filter(d => {
+    // console.log('wa', d);
+    return keepKeys.includes(`S${pad2(d.season)}-E${pad2(d.episode)}`)
+  })
+  // .tap(plzl => console.log('ya', plzl))
+  .reduce((map, val) => {
+    const key = _.pick(val, ['from_char', 'to_char'])
+    if( ! map.has(key) ) {
+      map.set(key, val.val)
+    } else {
+      const c = map.get(key)
+      map.set(key, c+val.val)
+    }
+    return map
+  }, new Map())
+
+  const plz2 = Array.from(plz)
+  .map(d => {
+    return {
+      source: d[0].from_char,
+      target: d[0].to_char,
+      value: d[1]
+    }
+  })
+
+  // console.log('val', plz2);
+  m.end().log()
+  return plz2
+}
+
+export const charOccNodes = (state, getters) => {
+  const m = new Measure('charOccNodes')
+  m.start()
+  const { charCooccurrence } = getters
+  const nodes = _.uniq(charCooccurrence.map(d => d.source).concat(charCooccurrence.map(d => d.target)))
+  .map(d => {
+    const r = charCooccurrence.filter(item => item.target === d).length
+    return {
+      id: d,
+      r: Math.max(r+2, 3)
+    }
+  })
+
+  m.end().log()
+  return nodes
+}
