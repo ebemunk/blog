827cd37620652b523d93479dd0c110656dc46ee0
ebemunk <ebemunk@gmail.com>
2018-01-05 01:35:36 -0800

    hm

--- static/lost-transcript-mining/viz2/src/sections/CharWordHistogram/Interaction.jsx
+++ static/lost-transcript-mining/viz2/src/sections/CharWordHistogram/Interaction.jsx
@@ -11,6 +11,7 @@ export default class Interaction extends React.Component {
       bin: null,
       pctChars: null,
       pctWords: null,
+      numChars: null,
     }
   }
 
@@ -32,6 +33,7 @@ export default class Interaction extends React.Component {
                 bin: (scales.x(p.data.x0) + scales.x(p.data.x1)) / 2,
                 pctChars: scales.y2(derivedData.cumChars[i].y),
                 pctWords: scales.y2(derivedData.cumWords[i].y),
+                numChars: scales.y1(p.data.length)
               },
               visible: true
             })}
@@ -46,6 +48,13 @@ export default class Interaction extends React.Component {
               y2={data.pctChars}
               className={style.pctCharsLine}
             />
+            <text
+              children={Math.round(data.pctChars)}
+              className={style.pctCharsVal}
+              x={width + 6}
+              y={data.pctChars}
+            />
+
             <line
               x1={data.bin}
               y1={data.pctWords}
@@ -53,6 +62,13 @@ export default class Interaction extends React.Component {
               y2={data.pctWords}
               className={style.pctWordsLine}
             />
+            <text
+              children={Math.round(data.pctWords)}
+              className={style.pctWordsVal}
+              x={width + 6}
+              y={data.pctWords}
+            />
+
             <line
               x1={data.bin}
               y1={height}
@@ -60,6 +76,26 @@ export default class Interaction extends React.Component {
               y2={data.pctChars}
               className={style.binsLine}
             />
+            <text
+              children={Math.round(data.bin)}
+              className={style.binVal}
+              x={data.bin}
+              y={height + 6}
+            />
+
+            <line
+              x1={-6}
+              y1={data.numChars}
+              x2={data.bin}
+              y2={data.numChars}
+              className={style.numCharsLine}
+            />
+            {/* <text
+              children={Math.round(data.numChars)}
+              className={style.numCharsVal}
+              x={data.bin}
+              y={data.numChars}
+            /> */}
           </g>
         }
       </g>
---
+++ static/lost-transcript-mining/viz2/src/sections/CharWordHistogram/Labels.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import CharWordHistogram from './CharWordHistogram'
+
+export default function Labels(props) {
+
+}
