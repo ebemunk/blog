148c1c8688508636d1d9c219537df2f3d4100dc3
Bugra Firat <bfirat@xmatters.com>
2018-01-06 00:28:56 -0800

    after 555

--- static/lost-transcript-mining/viz2/src/sections/CharWordHistogram/Labels.jsx
+++ static/lost-transcript-mining/viz2/src/sections/CharWordHistogram/Labels.jsx
@@ -15,6 +15,14 @@ export default function Labels(props) {
         <div className={style.yLabelWrap}>
           <span className={style.rotatedLabel}>Percentage</span>
         </div>
+        <div className={style.legend}>
+          <div className={style.legendItem}>
+            <div className={style.charLegend}></div> Cumulative Percentage of Characters
+          </div>
+          <div className={style.legendItem}>
+            <div className={style.wordLegend}></div> Cumulative Percentage of Words
+          </div>
+        </div>
       </div>
       <div className={style.xLabelWrap}>
         <span className={style.centeredLabel}>Words</span>
