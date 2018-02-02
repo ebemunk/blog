15b15232f5b9f0e8e303c52fc1efd1c03baa0376
Nathan DeVuono <nathan.devuono@gmail.com>
2018-02-02 14:26:44 -0800

    Ejected from create-react-app, we have a map and markers

--- static/van-eventviz/ui/src/components/Map/Map.jsx
+++ static/van-eventviz/ui/src/components/Map/Map.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import cnb from "classnames/bind";
+
+import eventsJsonRaw from "./eventsWithGeo.json";
+
+import { Marker } from "../../components";
 
 import styles from "./Map.styles.scss";
 
 const cn = cnb.bind(styles);
 
-export default function Map() {
-  return (
-    <div className={cn("container")}>
-      <GoogleMapReact defaultCenter={{ lat: 59.95, lng: 30.33 }} zoom={11} />
-    </div>
-  );
+export default class Map extends Component {
+  state = {
+    location: { coords: {}, events: [] }
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.setLocation);
+    this.setState({
+      events:
+        eventsJsonRaw[
+          "select * from events where geo is not null and details is not null"
+        ]
+    });
+  }
+
+  setLocation = location => {
+    console.log(location);
+    this.setState({ location });
+  };
+
+  render() {
+    const {
+      location: { coords: { latitude, longitude } },
+      events
+    } = this.state;
+    return (
+      <div className={cn("container")}>
+        <GoogleMapReact center={{ lat: latitude, lng: longitude }} zoom={12}>
+          {events &&
+            events.map(event => {
+              const { geo, id } = event;
+              return (
+                <Marker
+                  key={id}
+                  lat={JSON.parse(geo).results[0].geometry.location.lat}
+                  lng={JSON.parse(geo).results[0].geometry.location.lng}
+                />
+              );
+            })}
+        </GoogleMapReact>
+        )}
+      </div>
+    );
+  }
 }
--- static/van-eventviz/ui/src/components/index.js
+++ static/van-eventviz/ui/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Map } from "./Map";
+export { default as Marker } from "./Marker";
