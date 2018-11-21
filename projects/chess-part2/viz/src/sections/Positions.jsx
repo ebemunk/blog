2cd06c32815b10385c8bb8788ac39f57622b1a33
ebemunk <ebemunk@gmail.com>
2018-11-20 22:40:38 -0800

    ok

--- projects/chess-part2/viz/src/sections/Positions.jsx
+++ projects/chess-part2/viz/src/sections/Positions.jsx
@@ -7,8 +7,8 @@ import Board from '../components/Chess/Board'
 import Piece from '../components/Chess/Piece'
 import { squareFromIndex } from '../components/Chess/util'
 
-const fenToArr = fen => {
-  const squares = fen
+const fenToArr = fen =>
+  fen
     .match(/(([PpNnBbRrQqKk1-8]+(\/?))+) [bw] .+/)[1]
     .split('/')
     .reduce((acc, val) => acc.concat(val.split('')), [])
@@ -21,19 +21,17 @@ const fenToArr = fen => {
       }
     }, [])
 
-  console.log('a', squares)
-  return squares
-}
-
 const Positions = ({}) => {
-  const datz = data.Positions.slice().sort((a, b) => b.y - a.y)
+  const datz = data.Positions.slice()
+    .filter(d => +d.x[d.x.length - 1] > 5)
+    .sort((a, b) => b.y - a.y)
 
   return (
     <div>
-      {datz.slice(0, 10).map(d => (
+      {datz.slice(0, 30).map(d => (
         <div key={d.x}>
           {d.y}: {d.x}
-          <Board width={200}>
+          {/* <Board width={200}>
             {fenToArr(d.x).map(
               (piece, i) =>
                 !!piece && (
@@ -41,10 +39,11 @@ const Positions = ({}) => {
                     type={piece}
                     square={squareFromIndex(i).square}
                     width={200 / 8}
+                    key={squareFromIndex(i).square}
                   />
                 ),
             )}
-          </Board>
+          </Board> */}
         </div>
       ))}
     </div>
