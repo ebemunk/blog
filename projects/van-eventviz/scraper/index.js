b0a0dec83b934d56498cd5f364a1b0e533c68897
ebemunk <ebemunk@gmail.com>
2018-11-21 23:28:20 -0800

    better geo from places where address is empty

--- projects/van-eventviz/scraper/index.js
+++ projects/van-eventviz/scraper/index.js
@@ -4,6 +4,7 @@ import packageJson from './package.json'
 import scrape from './src/scrape'
 import details from './src/details'
 import writeJson from './src/writeJson'
+import placeGeo from './src/placeGeo'
 
 caporal
   .version(packageJson.version)
@@ -16,5 +17,8 @@ caporal
 
   .command('writeJson', 'write json to myjson.com')
   .action(writeJson)
+
+  .command('placeGeo', 'attempt to geocode from place name')
+  .action(placeGeo)
 
 caporal.parse(process.argv)
