c4b67b26b401abadedd047778d7fad01f6c8abba
Bugra Firat <bfirat@xmatters.com>
2018-12-07 13:17:53 -0800

    responsive dow

--- projects/year-in-van/src/roots/DayOfWeek/index.js
+++ projects/year-in-van/src/roots/DayOfWeek/index.js
@@ -4,8 +4,6 @@ import { format } from 'd3'
 import dow from 'data/dow'
 import month from 'data/month'
 
-console.log('wa', month)
-
 import { BarChart, Labels } from 'vizlib'
 
 import css from './DayOfWeek.css'
