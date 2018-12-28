03bd2ab4224934fdc7334ebd912c8c513ec57ae7
Bugra Firat <bfirat@xmatters.com>
2018-12-28 14:49:12 -0800

    update starter

--- projects/starter/webpack.config.js
+++ projects/starter/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const webpack = require('webpack')
-// const PostcssPresetEnv = require('postcss-preset-env')
+const PostcssPresetEnv = require('postcss-preset-env')
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -36,40 +36,46 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules|vizlib/,
       },
-      // {
-      //   test: /\.css$/,
-      //   use: [
-      //     'style-loader',
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         importLoaders: 1,
-      //         modules: true,
-      //         localIdentName: '[name]__[local]-[hash:base64:2]',
-      //       },
-      //     },
-      //     {
-      //       loader: 'postcss-loader',
-      //       options: {
-      //         ident: 'postcss',
-      //         plugins: [
-      //           PostcssPresetEnv({
-      //             stage: 0,
-      //             features: {
-      //               'color-mod-function': {
-      //                 unresolved: 'warn',
-      //               },
-      //             },
-      //           }),
-      //         ],
-      //       },
-      //     },
-      //   ],
-      // },
-      // {
-      //   test: /\.csv$/,
-      //   use: [{ loader: 'dsv-loader', options: { delimiter: ',' } }],
-      // },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]-[hash:base64:2]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                PostcssPresetEnv({
+                  stage: 0,
+                  features: {
+                    'color-mod-function': {
+                      unresolved: 'warn',
+                    },
+                  },
+                }),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.csv$/,
+        use: [{ loader: 'dsv-loader', options: { delimiter: ',' } }],
+      },
     ],
   },
 }
