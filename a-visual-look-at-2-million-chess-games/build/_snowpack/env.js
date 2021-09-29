9e97f4eb194947b75d4485d33319186c9f941e1e
ebemunk <ebemunk@gmail.com>
2021-09-28 21:26:31 -0700

    hugo rebuild

--- a-visual-look-at-2-million-chess-games/build/_snowpack/env.js
+++ a-visual-look-at-2-million-chess-games/build/_snowpack/env.js
@@ -1,1 +1,3 @@
-export const MODE="production";export const NODE_ENV="production";export const SSR=false;
+export const MODE = "production";
+export const NODE_ENV = "production";
+export const SSR = false;
